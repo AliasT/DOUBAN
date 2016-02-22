fc68437de7da79b42928dd54a35964f2620c0b0e
xiaobing <chaixb@lizi.com>
2016-02-23 00:00:29 +0800

    sublime自定义插件，选中文本生成数组

---
+++ sublime-build-array.py
@@ -0,0 +1,13 @@
+mport sublime, sublime_plugin
+import re
+class ECommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        # 唯一选区
+        selection = self.view.sel()[0]
+        print(self.view.substr(selection))
+
+        result = []
+        for m in re.finditer(r'\S+.+\S', self.view.substr(selection), re.M):
+            result.append('\"' + m.group(0) + '\"')
+
+        self.view.replace(edit, selection, ',\n'.join(result))
